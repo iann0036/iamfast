4411fb69be844a35738420e443f906ec15a73f5f
Ian Mckay <contact@ianmckay.com.au>
2022-03-01 20:57:53 +1100

    oh no

--- tests/python/test6.py
+++ tests/python/test6.py
@@ -1,5 +1,7 @@
 import boto3
+import boto3 as somethingelse
 from boto3.session import Session
+from boto3 import Session as sessionroot
 
 session = Session()
 s3 = session.resource('s3')
@@ -10,5 +12,18 @@ for s3_file in your_bucket.objects.all():
 
 s32 = boto3.resource('s3')
 
-your_bucket2 = s3.Bucket('yourbucket2')
+your_bucket2 = s32.Bucket('yourbucket2')
 print(your_bucket2.objects.all())
+
+session2 = boto3.Session()
+s33 = session2.client('s3')
+s33.list_objects(Bucket='yourbucket3')
+
+s34 = boto3.Session().resource('s3')
+s34.objects.all()
+
+s3name = 's3'
+s35 = Session().client(s3name)
+s35.list_objects_v2(Bucket='yourbucket3')
+
+boto3.Session().client('s3').list_objects_v2(Bucket='yourbucket3').keys()
